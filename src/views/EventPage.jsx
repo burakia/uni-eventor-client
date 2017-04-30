9337e5a8401093c16c65a03fb658c0b91ef61374
byaldira <burak_yaldirak@hotmail.com>
2017-04-30 23:05:52 +0300

    EventUsers- EventPage
    EventUsers- EventPage

--- src/views/EventPage.jsx
+++ src/views/EventPage.jsx
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import EventCardProfile from '../component/EventCardProfile.jsx';
 import EventCoomingUp from '../component/EventCoomingUp.jsx';
+import EventUsers from '../component/EventUsers.jsx';
 import '../css/w3.css';
 import '../css/w3-theme-black.css';
 
 class EventPage extends Component {
+    
    handleClick =(e,cityName)=>{
       // e burada click icin bildigimiz event nesnesi
-      alert('adsf');
+        var i, x, tablinks;
+            x = document.getElementsByClassName("city");
+            for (i = 0; i < x.length; i++) {
+                x[i].style.display = "none";
+            }
+            tablinks = document.getElementsByClassName("tablink");
+            for (i = 0; i < x.length; i++) {
+                tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
+            }
+            document.getElementById(e).style.display = "block";
+            cityName.currentTarget.firstElementChild.className += " w3-border-red";
+      
     }
+     
     render() {
-        
+       
         return (
             <div className="w3-row">
                 <div className="w3-col m3">
@@ -45,14 +59,15 @@ class EventPage extends Component {
                                            
                                                 <h4 >Saü Bilişim Günleri</h4>
                                                 <div className="w3-row">
-                                                    <a href="javascript:void(0)" onClick={(e) => this.handleClick.bind(this,'Icerik')}>
+                                                    <a href="javascript:void(0)" onClick={(e) => this.handleClick('Icerik')}>
                                                         <div className="w3-third tablink w3-bottombar w3-hover-light-grey w3-padding">İçerik</div>
                                                     </a>
-                                                    <a href="javascript:void(0)" onClick={(e) => this.handleClick.bind(this,'Harita')}>
+                                                    <a href="javascript:void(0)" onClick={(e) => this.handleClick('Harita')}>
                                                         <div className="w3-third tablink w3-bottombar w3-hover-light-grey w3-padding">Harita</div>
                                                     </a>
                                                 </div>
-                                                
+                                             
+
                                              </div>
                                       </div>
                                 </div>
@@ -61,12 +76,7 @@ class EventPage extends Component {
                  <div className="w3-col m2">
                     <EventCoomingUp poster="https://about.canva.com/wp-content/uploads/sites/3/2015/01/concert_poster.png"
                                     title="Event S 1" id="s2" />
-                     <div className="w3-card-2 w3-round w3-white w3-center">
-                        <div className="w3-container">
-                            <p>Bu Etkinliğe Gidenler</p>
-                               <span>Jane Doe</span>
-                        </div>
-                    </div>
+                    <EventUsers/>
                  </div>
             </div>
         );
