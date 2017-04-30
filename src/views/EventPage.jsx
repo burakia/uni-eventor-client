d4629b52032e857791cecd9f915dc79861282c7a
byaldira <burak_yaldirak@hotmail.com>
2017-05-01 00:34:32 +0300

    EventTabs
    EventTabs

--- src/views/EventPage.jsx
+++ src/views/EventPage.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import EventCardProfile from '../component/EventCardProfile.jsx';
 import EventCoomingUp from '../component/EventCoomingUp.jsx';
 import EventUsers from '../component/EventUsers.jsx';
+import EventMapFrame from '../component/EventMapFrame.jsx';
+
 import '../css/w3.css';
 import '../css/w3-theme-black.css';
 
@@ -19,18 +21,18 @@ class EventPage extends Component {
                 tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
             }
             document.getElementById(e).style.display = "block";
-            cityName.currentTarget.firstElementChild.className += " w3-border-red";
+            e.currentTarget.firstElementChild.className += " w3-border-red";
       
     }
      
     render() {
-       
+     
         return (
             <div className="w3-row">
                 <div className="w3-col m3">
                      <EventCardProfile poster="https://about.canva.com/wp-content/uploads/sites/3/2015/01/concert_poster.png"
                                     title="Event S 1" id="s1" />
-                     <div class="w3-card-2 w3-round">
+                     <div className="w3-card-2 w3-round">
                         Accordion
                     </div>
                      <div className="w3-card-2 w3-round w3-white w3-hide-small">
@@ -59,14 +61,26 @@ class EventPage extends Component {
                                            
                                                 <h4 >Saü Bilişim Günleri</h4>
                                                 <div className="w3-row">
-                                                    <a href="javascript:void(0)" onClick={(e) => this.handleClick('Icerik')}>
+                                                    <a href="javascript:void(0)" onClick={(e) => this.handleClick('Icerik',this).bind(this)}>
                                                         <div className="w3-third tablink w3-bottombar w3-hover-light-grey w3-padding">İçerik</div>
                                                     </a>
-                                                    <a href="javascript:void(0)" onClick={(e) => this.handleClick('Harita')}>
+                                                    <a href="javascript:void(0)" onClick={(e) => this.handleClick('Harita',this).bind(this)}>
                                                         <div className="w3-third tablink w3-bottombar w3-hover-light-grey w3-padding">Harita</div>
                                                     </a>
                                                 </div>
-                                             
+                                                <div id="Icerik" ref="city" className="w3-container city">
+                                                    <h2>Icerik</h2>
+                                                    <p>Global markalar senin için kampüse geliyor ve markalaşmayı anlatıyor! Konu başlıklarımız neler? 
+                                                    ? Globalleşen dünyada değeri gitgide artan ?markalaşmayı? anlamak. 
+                                                    ? Markalaşan başarılı şirketlerin hikayelerini dinlemek, ilham almak. 
+                                                    ? Aktif olarak iş hayatında yıllarını harcamış başarılı yöneticilerin tecrübelerini, kariyerine henüz başlamakta olan gençlerle paylaşması.
+                                                    ? Global markalaşma nedir, nasıl olur bunları global markalardan dinlemek. 
+                                                    ? Nasıl bir kariyer haritası izleyeceğini bilemeyen öğrencilere ilham vermek.
+                                                    Katılımcı Markalar, Koç Bilişim Mercedes Metro UPS</p>
+                                                </div>
+                                                <div id="Harita" ref="city" className="w3-container city" >
+                                                    <EventMapFrame lat="40.7413232" lon="30.3296314"/>
+                                                 </div>
 
                                              </div>
                                       </div>
@@ -76,6 +90,7 @@ class EventPage extends Component {
                  <div className="w3-col m2">
                     <EventCoomingUp poster="https://about.canva.com/wp-content/uploads/sites/3/2015/01/concert_poster.png"
                                     title="Event S 1" id="s2" />
+                                    <br></br>
                     <EventUsers/>
                  </div>
             </div>
