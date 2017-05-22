6c4bdf57c003bd3e236b5d85f93cdd6e2f003639
burakia <burakkyilmazz_95@hotmail.com>
2017-05-22 22:46:46 +0300

    Slider çalısmıyor

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -3,47 +3,43 @@ import SearchBar from '../component/SearchBar.jsx';
 import EventCardSmall from '../component/EventCardSmall.jsx';
 import EventCardMedium from '../component/EventCardMedium.jsx';
 import '../css/w3.css';
-
+import Slider from 'react-image-slider';
 
 class HomePage extends Component {
 
-
     render() {
-        var sliderStyle = {
-            width: '%100',
-            height: '400px'
-        };
+        const images = [
+            'http://payload.cargocollective.com/1/4/146823/2056344/DrumbeatPosterRGB-01-01.jpg',
+            'http://payload.cargocollective.com/1/4/146823/2056344/DrumbeatPosterRGB-01-01.jpg'
+        ];
         return (
             <div>
-                 
-                <div id="slider" className="w3-gray" style={sliderStyle}>
-                    <div className="w3-content">
-                    <h1 className="w3-xxlarge">Slider</h1>
-                    </div>
+                <div>
+                    <Slider images={images} isInfinite delay={5000}>
+                        {images.map((image, key) => <div key={key}><img src={image} /></div>)}
+                    </Slider>
                 </div>
                 <SearchBar />
                 <br />
-                
                 <div className="w3-content">
                     <div className="w3-container"><h2>İlginizi Çekebilecek Etkinlikler</h2></div>
-                    
+
                     <EventCardSmall poster="https://about.canva.com/wp-content/uploads/sites/3/2015/01/concert_poster.png"
-                                    title="Event S 1" id="s1" />
+                        title="Event S 1" id="s1" />
                     <EventCardSmall poster="https://about.canva.com/wp-content/uploads/sites/3/2015/01/concert_poster.png"
-                                    title="Event S 2" id="s2" />
+                        title="Event S 2" id="s2" />
                     <EventCardSmall poster="https://about.canva.com/wp-content/uploads/sites/3/2015/01/concert_poster.png"
-                                    title="Event S 3" id="s3" />
+                        title="Event S 3" id="s3" />
                     <EventCardSmall poster="https://about.canva.com/wp-content/uploads/sites/3/2015/01/concert_poster.png"
-                                    title="Event S 4" id="s4" />
+                        title="Event S 4" id="s4" />
                     <div className="w3-container"><h2>Öne Çıkan Etkinlikler</h2></div>
                     <EventCardMedium poster="https://about.canva.com/wp-content/uploads/sites/3/2015/01/concert_poster.png"
-                                    title="Event M 1" id="s1" />
+                        title="Event M 1" id="s1" />
                     <EventCardMedium poster="https://about.canva.com/wp-content/uploads/sites/3/2015/01/concert_poster.png"
-                                    title="Event M 2" id="m2" />
+                        title="Event M 2" id="m2" />
                     <EventCardMedium poster="https://about.canva.com/wp-content/uploads/sites/3/2015/01/concert_poster.png"
-                                    title="Event M 3" id="m3" />
+                        title="Event M 3" id="m3" />
                 </div>
-                
             </div>
         );
     }
